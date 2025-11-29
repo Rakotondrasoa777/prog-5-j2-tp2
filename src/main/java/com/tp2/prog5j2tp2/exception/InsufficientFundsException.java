4b1fadea85a4230edab0bb3bcdf247ee2d94ce05
Rakotondrasoa777 <rakotondrasoatojonandrianina5@gmail.com>
2025-11-29 23:35:09 +0300

    feat: add exception

---
+++ src/main/java/com/tp2/prog5j2tp2/exception/InsufficientFundsException.java
@@ -0,0 +1,7 @@
+package com.tp2.prog5j2tp2.exception;
+
+public class InsufficientFundsException extends RuntimeException {
+    public InsufficientFundsException(String message) {
+        super(message);
+    }
+}
