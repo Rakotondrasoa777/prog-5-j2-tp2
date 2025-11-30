d593c97632f3c5478e92322014a727dbe03c5361
Rakotondrasoa777 <rakotondrasoatojonandrianina5@gmail.com>
2025-11-30 14:52:54 +0300

    feat: add new controller

--- src/main/java/com/tp2/prog5j2tp2/controller/SimpleRentalController.java
+++ src/main/java/com/tp2/prog5j2tp2/controller/SimpleRentalController.java
@@ -2,16 +2,18 @@ package com.tp2.prog5j2tp2.controller;
 
 import com.tp2.prog5j2tp2.entity.Items;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.tp2.prog5j2tp2.service.SimpleRentalService;
 
 import java.util.List;
 
 @RestController
-public class SImpleRentalController {
+public class SimpleRentalController {
     public final SimpleRentalService simpleRentalService;
 
-    public SImpleRentalController(SimpleRentalService simpleRentalService) {
+    public SimpleRentalController(SimpleRentalService simpleRentalService) {
         this.simpleRentalService = simpleRentalService;
     }
 
@@ -24,5 +26,15 @@ public class SImpleRentalController {
     @GetMapping("/simplerental/items")
     public List<Items> getItemsOfCLient() {
         return simpleRentalService.showAllItemsOfClient();
+    }
+
+    @PostMapping("/simplerental/items/add")
+    public void addItemToCLient(@RequestBody Items itemToAdd) {
+        simpleRentalService.addItemRented(itemToAdd);
+    }
+
+    @PostMapping("/simplerental/pay")
+    public void pay() {
+        simpleRentalService.payRental();
     }
 }
